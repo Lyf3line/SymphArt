99e39e54328f06cf958823d2486401889d98d5c0
Bas Klein <bas-klein@live.nl>
2018-11-22 17:43:16 +0100

    Added form

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,15 @@
 
 	use App\Entity\Article;
 	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\HttpFoundation\Request;
+
 	use Symfony\Component\Routing\Annotation\Route;
 	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	
+	use Symfony\Component\Form\Extension\Core\Type\TextType;
+	use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+	use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 
 	class ArticleController extends Controller{
@@ -26,19 +32,42 @@
 		return $this->render('articles/index.html.twig', array('articles' => $articles));
 		}
 
+
+		/**
+		* @Route("/article/new", name="new_article")
+		* Method({"GET", "POST"})
+		*/
+
+		public function new(Request $request){
+
+			$article = new Article();
+
+			$form = $this->createFormBuilder($article)
+			->add('title', TextType::class, array('attr' => array('class' => 'form-control')))
+			->add('body', TextareaType::class, array('required' => false, 'attr' => array('class' => 'form-control')))
+			->add('save', SubmitType::class, array('label' => 'Create', 'attr' => array('class' => 'btn btn-primary mt-3')))
+			->getForm();
+
+			return $this->render('articles/new.html.twig', array(
+				'form' => $form->createView()
+			));
+
+		}
+
 		/**
 		* @Route("/article/{id}", name="article_show")
 		*/
 
 		public function show($id){
 
-		// Call function to show a specific article by id.
+			// Call function to show a specific article by id.
 
-		$article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+			$article = $this->getDoctrine()->getRepository(Article::class)->find($id);
 
-		//Get the results and pass them to the view. In the form of an array.
-		return $this->render('articles/show.html.twig', array('article' => $article));
+			//Get the results and pass them to the view. In the form of an array.
+			return $this->render('articles/show.html.twig', array('article' => $article));
 		}
+
 
 		/**
 		 * @Route("/article/save")
